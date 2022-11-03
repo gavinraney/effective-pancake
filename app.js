625208d9f62225974141d93e3ead54a34dba1787
Peyton <prjunjulas@gmail.com>
2022-11-02 21:06:41 -0500

    added index page and more

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const bodyparser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+dotenv.config({path:'config.env'});
+const PORT = process.env.PORT || 8080
+
+//parse requests to bp
+app.use(bodyparser.urlencoded({ extended:true }))
+
+//setting view engine
+app.set("view engine", "ejs")
+
+//load assets
+app.use('/css',express.static(path.resolve(__dirname,"assets/css")))
+app.use('/js',express.static(path.resolve(__dirname,"assets/js")))
+
+app.get('/', (req,res)=>{
+    res.render('index');
+})
+
+app.get('/show_food', (req,res)=>{
+    res.render('show_food');
+})
+
+app.get('/questions', (req,res)=>{
+    res.render('questions');
+})
+
+// app.listen(PORT, ()=>{console.log(`Server is running on http://localhost:${PORT}`)});
+
+app.listen(process.env.PORT || 3000, 
+    () => console.log("server running"));
