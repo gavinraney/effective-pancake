891ff0bba0ab1e7d161d1e6bb73694c28124edea
Kobyingram11 <kobyingram11@gmail.com>
2022-11-17 12:40:32 -0600

    new .env files

--- app.js
+++ app.js
@@ -1,77 +1,85 @@
 require("dotenv").config();
-const express = require('express');
-// const dotenv = require('dotenv');
-const bodyParser = require('body-parser');
 const path = require('path');
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
 const { urlencoded } = require('body-parser');
-// const MongoClient = require('mongodb').MongoClient
+const { ObjectId } = require('mongodb')
+const PORT = (process.env.PORT || 5000);
+// const herokuVar = process.env.HEROKU_NAME || "kingram"
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const client = new MongoClient(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-const app = express();
-const { ObjectId } = require('mongodb');
-
-//setting view engine
-app.set("view engine", "ejs")
-
-// dotenv.config({ path: 'config.env' });
-
 app.use(bodyParser.urlencoded({ extended: true }))
+app.set('view engine', 'ejs')
+app.use(bodyParser.json());
+app.use(express.static('public'))
 
-// dotenv.config({ path: 'config.env' });
-//const PORT = process.env.PORT || 8080
+// let someVar = "";
 
-console.log('here');
-console.log(process.env.MONGO_URI);
+async function cxnDB(){
+    try{
+      client.connect;
+      const collection = client.db("wwe").collection("restaurants");
+      // const result = await collection.dropIndex().toArray();
+  
+      console.log("cxnDB result:", result);
+      return result;
+  
+    }
+    catch(e){
+      console.log(e)
+    }
+    finally{
+      client.close;
+    }
+  }
+  
 
-app.get('/', async function (req, res) {
 
-
-    // client.connect; 
-    console.log('connected'); 
-
-    res.render('index');
-   })
-
-const db = client.db('restaurants');
-const restaurantsCollection = db.collection('restaurants');
-
-    //parse requests to bp
-    app.use(bodyParser.urlencoded({ extended: true }))
-    app.use(bodyParser.json());
-    app.use(express.static('public'));
-
+  app.get('/', async (req, res) => {
+  
+    let restaurantData = await cxnDB().catch(console.error);
+    console.log("get/:", restaurantData); 
+    res.render('index.ejs', {
+       
+       })
+  })
 
 
         // load assets
-         app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
-         app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
+        app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
+        app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 
-         app.get('/', (req, res) => {
+         app.get('/', async (req, res) => {
              res.render('index');
          })
 
-         app.get('/show_food', (req, res) => {
+         app.get('/show_food', async (req, res) => {
              res.render('show_food');
          })
 
-         app.get('/questions', (req, res) => {
+         app.get('/questions', async (req, res) => {
              res.render('questions');
          })
 
-         app.get('/aboutus', (req, res) => {
+         app.get('/aboutus', async (req, res) => {
              res.render('aboutus');
          })
 
-MongoClient.connect(process.env.MONGO_URI, {
-    useUnifiedTopology: true
-})
-    .then(client => {
-        console.log('connected to db');
+// MongoClient.connect(process.env.MONGO_URI, {
+   // useUnifiedTopology: true
+// })
+   // .then(client => {
+     //   console.log('connected to db');
 
-        app.listen(process.env.PORT || 3000,
-            () => console.log("server running"));
+     //   app.listen(process.env.PORT || 3000,
+       //     () => console.log("server running"));
 
-    })
-    .catch(error => console.error(error));
+   // })
+   // .catch(error => console.error(error));
 
-    // app.listen(PORT, ()=>{console.log(`Server is running on http://localhost:${PORT}`)});
+
+    app.listen(process.env.PORT || 5000, function() {
+        console.log(`Server is running & listening on port ${PORT}`); 
+      
+      });
