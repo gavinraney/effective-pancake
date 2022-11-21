3cfa17f4f46441284d6db25f78fc09520f89375e
Peyton <prjunjulas@gmail.com>
2022-11-21 11:55:47 -0600

    this is a good commit (hopefully)

--- app.js
+++ app.js
@@ -1,85 +1,71 @@
-require("dotenv").config();
-const path = require('path');
-const express = require('express')
-const app = express()
+const express = require('express');
+const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
-const { urlencoded } = require('body-parser');
-const { ObjectId } = require('mongodb')
-const PORT = (process.env.PORT || 5000);
-// const herokuVar = process.env.HEROKU_NAME || "kingram"
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const client = new MongoClient(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-app.use(bodyParser.urlencoded({ extended: true }))
-app.set('view engine', 'ejs')
+const path = require('path');
+const MongoClient = require('mongodb').MongoClient
+const app = express();
+
+dotenv.config({path:'config.env'});
+//const PORT = process.env.PORT || 8080
+
+//parse requests to bp
+app.use(bodyParser.urlencoded({ extended:true }))
+
+
+MongoClient.connect(process.env.MONGO_URI, {
+    useUnifiedTopology: true })
+    .then(client =>{
+    console.log("db connected");
+    const db = client.db('wwe')
+    const restCollections = db.collection('restaurants')
+
+//setting view engine
+app.set("view engine", "ejs");
 app.use(bodyParser.json());
-app.use(express.static('public'))
 
-// let someVar = "";
+//load assets
+ app.use('/css',express.static(path.resolve(__dirname,"assets/css")))
+ app.use('/js',express.static(path.resolve(__dirname,"assets/js")))
 
-async function cxnDB(){
-    try{
-      client.connect;
-      const collection = client.db("wwe").collection("restaurants");
-      const result = await collection.createIndex().toArray();
-  
-      console.log("cxnDB result:", result);
-      return result;
-  
-    }
-    catch(e){
-      console.log(e)
-    }
-    finally{
-      client.close;
-    }
-  }
-  
+//app.use(express.static(path.join(__dirname, "assets")));
+
+//app.use(express.static('assets'));
+
+app.get('/', (req,res)=>{
+    res.render('index');
+})
 
 
-  app.get('/', async (req, res) => {
-  
-    let restaurantData = await cxnDB().catch(console.error);
-    console.log("/:", restaurantData); 
-    res.render('index.ejs', {
-       
-       })
-  })
+app.get('/show_food/:meat', (req,res)=>{
+    // res.render('show_food');
+    console.log(req.params.meat);
+    db.collection('restaurants').find({
+        meat: req.params.meat
+    }).toArray()
+        .then(results => {
+            res.render('show_food', {restaurants: results})
+            console.log(results)
+        }) 
+        .catch(error => console.error(error))
+
+})
 
 
-        // load assets
-        app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
-        app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
+app.get('/questions', (req,res)=>{
+    res.render('questions');
+})
 
-         app.get('/', async (req, res) => {
-             res.render('index');
-         })
+app.get('/accept/:r_name', (req,res)=>{
+    res.render('accept');
+})
 
-         app.get('/show_food', async (req, res) => {
-             res.render('show_food');
-         })
-
-         app.get('/questions', async (req, res) => {
-             res.render('questions');
-         })
-
-         app.get('/aboutus', async (req, res) => {
-             res.render('aboutus');
-         })
-
-// MongoClient.connect(process.env.MONGO_URI, {
-   // useUnifiedTopology: true
-// })
-   // .then(client => {
-     //   console.log('connected to db');
-
-     //   app.listen(process.env.PORT || 3000,
-       //     () => console.log("server running"));
-
-   // })
-   // .catch(error => console.error(error));
+app.get('/aboutus', (req,res)=>{
+    res.render('aboutus');
+ })
 
 
-    app.listen(process.env.PORT || 5000, function() {
-        console.log(`Server is running & listening on port ${PORT}`); 
-      
-      });
+app.listen(process.env.PORT || 5000, 
+    () => console.log("server running"));
+
+})
+.catch(error => console.error(error))
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,75 +1,76 @@
-var b_count = 0;
-const btn_b = document.getElementById('beef');
-btn_b.addEventListener('click', function () {
-    if (b_count == 0){
-        
-        btn_b.style.backgroundColor = 'white';
-        btn_b.style.border = '3px dashed black';
-        btn_b.style.color = 'white';
-        b_count = 1;
-        
-        //could possibly set the value for db querry here as well 
-    }
-    else{
-        
-        btn_b.style.backgroundColor = 'white';
-        btn_b.style.border = '3px dashed black';
-        btn_b.style.color = 'white';
-        b_count = 0;
-        
-    }
-});
+var meatType = "";
 
-var c_count = 0;
-const btn_c = document.getElementById('chicken');
-btn_c.addEventListener('click', function onClick() {
-    if (c_count == 0){
-        btn_c.style.backgroundColor = 'black';
-        btn_c.style.border = '3px dashed black';
-        btn_c.style.color = 'white';
-        c_count = 1;
-        //could possibly set the value for db querry here as well 
+const btn_b = document.getElementById('b');
+$("#b").click(function() {
+    $("#b").toggleClass("active");
+    if($("#b").hasClass("active")){
+        btn_b.style.backgroundColor = 'red';
+        btn_b.style.border = '3px dashed yellow';
+        btn_b.style.color = 'white';
+        console.log("button clicked");
+        meatType = $(this).attr("id");
+        console.log(meatType);
+    }else{
+        btn_b.style.backgroundColor = 'yellow';
+        btn_b.style.border = '3px dashed red';
+        btn_b.style.color = 'black';
+        console.log("button unclicked");
     }
-    else{
-        btn_c.style.backgroundColor = 'black';
-        btn_c.style.border = '3px dashed black';
-        btn_c.style.color = 'white';
-        c_count = 0;
-    }
-});
+})
 
-var p_count = 0;
-const btn_p = document.getElementById('pork');
-btn_p.addEventListener('click', function onClick() {
-    if (p_count == 0){
-        btn_p.style.backgroundColor = 'black';
-        btn_p.style.border = '3px dashed black';
-        btn_p.style.color = 'white';
-        p_count = 1;
-        //could possibly set the value for db querry here as well 
+
+const btn_c = document.getElementById('c');
+$("#c").click(function() {
+    $("#c").toggleClass("active");
+    if($("#c").hasClass("active")){
+        btn_c.style.backgroundColor = 'red';
+        btn_c.style.border = '3px dashed yellow';
+        btn_c.style.color = 'white';
+        console.log("button clicked");
+        meatType = $(this).attr("id");
+        console.log(meatType);
+    }else{
+        btn_c.style.backgroundColor = 'yellow';
+        btn_c.style.border = '3px dashed red';
+        btn_c.style.color = 'black';
+        console.log("button unclicked");
     }
-    else{
-        btn_p.style.backgroundColor = 'black';
-        btn_p.style.border = '3px dashed black';
+})
+
+
+const btn_p = document.getElementById('p');
+$("#p").click(function() {
+    $("#p").toggleClass("active");
+    if($("#p").hasClass("active")){
+        btn_p.style.backgroundColor = 'red';
+        btn_p.style.border = '3px dashed yellow';
         btn_p.style.color = 'white';
-        p_count = 0;
+        console.log("button clicked");
+        meatType = $(this).attr("id");
+        console.log(meatType);
+    }else{
+        btn_p.style.backgroundColor = 'yellow';
+        btn_p.style.border = '3px dashed red';
+        btn_p.style.color = 'black';
+        console.log("button unclicked");
     }
-});
+})
+
 
 var m_count = 0;
 const btn_m = document.getElementById('mexican');
 btn_m.addEventListener('click', function onClick() {
     if (m_count == 0){
-        btn_m.style.backgroundColor = 'black';
-        btn_m.style.border = '3px dashed black';
+        btn_m.style.backgroundColor = 'red';
+        btn_m.style.border = '3px dashed yellow';
         btn_m.style.color = 'white';
         m_count = 1;
         //could possibly set the value for db querry here as well 
     }
     else{
-        btn_m.style.backgroundColor = 'black';
-        btn_m.style.border = '3px dashed black';
-        btn_m.style.color = 'white';
+        btn_m.style.backgroundColor = 'yellow';
+        btn_m.style.border = '3px dashed red';
+        btn_m.style.color = 'black';
         m_count = 0;
     }
 });
@@ -78,16 +79,16 @@ var i_count = 0;
 const btn_i = document.getElementById('italian');
 btn_i.addEventListener('click', function onClick() {
     if (i_count == 0){
-        btn_i.style.backgroundColor = 'black';
-        btn_i.style.border = '3px dashed black';
+        btn_i.style.backgroundColor = 'red';
+        btn_i.style.border = '3px dashed yellow';
         btn_i.style.color = 'white';
         i_count = 1;
         //could possibly set the value for db querry here as well 
     }
     else{
-        btn_i.style.backgroundColor = 'black';
-        btn_i.style.border = '3px dashed black';
-        btn_i.style.color = 'white';
+        btn_i.style.backgroundColor = 'yellow';
+        btn_i.style.border = '3px dashed red';
+        btn_i.style.color = 'black';
         i_count = 0;
     }
 });
@@ -96,16 +97,16 @@ var a_count = 0;
 const btn_a = document.getElementById('american');
 btn_a.addEventListener('click', function onClick() {
     if (a_count == 0){
-        btn_a.style.backgroundColor = 'black';
-        btn_a.style.border = '3px dashed black';
+        btn_a.style.backgroundColor = 'red';
+        btn_a.style.border = '3px dashed yellow';
         btn_a.style.color = 'white';
         a_count = 1;
         //could possibly set the value for db querry here as well 
     }
     else{
-        btn_a.style.backgroundColor = 'black';
-        btn_a.style.border = '3px dashed black';
-        btn_a.style.color = 'white';
+        btn_a.style.backgroundColor = 'yellow';
+        btn_a.style.border = '3px dashed red';
+        btn_a.style.color = 'black';
         a_count = 0;
     }
 });
@@ -114,16 +115,16 @@ var as_count = 0;
 const btn_as = document.getElementById('asian');
 btn_as.addEventListener('click', function onClick() {
     if (as_count == 0){
-        btn_as.style.backgroundColor = 'black';
-        btn_as.style.border = '3px dashed black';
+        btn_as.style.backgroundColor = 'red';
+        btn_as.style.border = '3px dashed yellow';
         btn_as.style.color = 'white';
         as_count = 1;
         //could possibly set the value for db querry here as well 
     }
     else{
-        btn_as.style.backgroundColor = 'black';
-        btn_as.style.border = '3px dashed black';
-        btn_as.style.color = 'white';
+        btn_as.style.backgroundColor = 'yellow';
+        btn_as.style.border = '3px dashed red';
+        btn_as.style.color = 'black';
         as_count = 0;
     }
 });
@@ -132,16 +133,16 @@ var one_count = 0;
 const btn_one = document.getElementById('1dollar');
 btn_one.addEventListener('click', function onClick() {
     if (one_count == 0){
-        btn_one.style.backgroundColor = 'black';
-        btn_one.style.border = '3px dashed black';
+        btn_one.style.backgroundColor = 'red';
+        btn_one.style.border = '3px dashed yellow';
         btn_one.style.color = 'white';
         one_count = 1;
         //could possibly set the value for db querry here as well 
     }
     else{
-        btn_one.style.backgroundColor = 'black';
-        btn_one.style.border = '3px dashed black';
-        btn_one.style.color = 'white';
+        btn_one.style.backgroundColor = 'yellow';
+        btn_one.style.border = '3px dashed red';
+        btn_one.style.color = 'black';
         one_count = 0;
     }
 });
@@ -150,16 +151,16 @@ var two_count = 0;
 const btn_two = document.getElementById('2dollar');
 btn_two.addEventListener('click', function onClick() {
     if (two_count == 0){
-        btn_two.style.backgroundColor = 'black';
-        btn_two.style.border = '3px dashed black';
+        btn_two.style.backgroundColor = 'red';
+        btn_two.style.border = '3px dashed yellow';
         btn_two.style.color = 'white';
         two_count = 1;
         //could possibly set the value for db querry here as well 
     }
     else{
-        btn_two.style.backgroundColor = 'black';
-        btn_two.style.border = '3px dashed black';
-        btn_two.style.color = 'white';
+        btn_two.style.backgroundColor = 'yellow';
+        btn_two.style.border = '3px dashed red';
+        btn_two.style.color = 'black';
         two_count = 0;
     }
 });
@@ -168,16 +169,16 @@ var three_count = 0;
 const btn_three = document.getElementById('3dollar');
 btn_three.addEventListener('click', function onClick() {
     if (three_count == 0){
-        btn_three.style.backgroundColor = 'black';
-        btn_three.style.border = '3px dashed black';
+        btn_three.style.backgroundColor = 'red';
+        btn_three.style.border = '3px dashed yellow';
         btn_three.style.color = 'white';
         three_count = 1;
         //could possibly set the value for db querry here as well 
     }
     else{
-        btn_three.style.backgroundColor = 'black';
-        btn_three.style.border = '3px dashed black';
-        btn_three.style.color = 'white';
+        btn_three.style.backgroundColor = 'yellow';
+        btn_three.style.border = '3px dashed red';
+        btn_three.style.color = 'black';
         three_count = 0;
     }
 });
@@ -186,16 +187,16 @@ var m_one_count = 0;
 const btn_m_one = document.getElementById('1m');
 btn_m_one.addEventListener('click', function onClick() {
     if (m_one_count == 0){
-        btn_m_one.style.backgroundColor = 'black';
-        btn_m_one.style.border = '3px dashed black';
+        btn_m_one.style.backgroundColor = 'red';
+        btn_m_one.style.border = '3px dashed yellow';
         btn_m_one.style.color = 'white';
         m_one_count = 1;
         //could possibly set the value for db querry here as well 
     }
     else{
-        btn_m_one.style.backgroundColor = 'black';
-        btn_m_one.style.border = '3px dashed black';
-        btn_m_one.style.color = 'white';
+        btn_m_one.style.backgroundColor = 'yellow';
+        btn_m_one.style.border = '3px dashed red';
+        btn_m_one.style.color = 'black';
         m_one_count = 0;
     }
 });
@@ -204,16 +205,16 @@ var m_three_count = 0;
 const btn_m_three = document.getElementById('3m');
 btn_m_three.addEventListener('click', function onClick() {
     if (m_three_count == 0){
-        btn_m_three.style.backgroundColor = 'black';
-        btn_m_three.style.border = '3px dashed black';
+        btn_m_three.style.backgroundColor = 'red';
+        btn_m_three.style.border = '3px dashed yellow';
         btn_m_three.style.color = 'white';
         m_three_count = 1;
         //could possibly set the value for db querry here as well 
     }
     else{
-        btn_m_three.style.backgroundColor = 'black';
-        btn_m_three.style.border = '3px dashed black';
-        btn_m_three.style.color = 'white';
+        btn_m_three.style.backgroundColor = 'yellow';
+        btn_m_three.style.border = '3px dashed red';
+        btn_m_three.style.color = 'black';
         m_three_count = 0;
     }
 });
@@ -222,16 +223,16 @@ var m_five_count = 0;
 const btn_m_five = document.getElementById('5m');
 btn_m_five.addEventListener('click', function onClick() {
     if (m_five_count == 0){
-        btn_m_five.style.backgroundColor = 'black';
-        btn_m_five.style.border = '3px dashed black';
+        btn_m_five.style.backgroundColor = 'red';
+        btn_m_five.style.border = '3px dashed yellow';
         btn_m_five.style.color = 'white';
         m_five_count = 1;
         //could possibly set the value for db querry here as well 
     }
     else{
-        btn_m_five.style.backgroundColor = 'black';
-        btn_m_five.style.border = '3px dashed black';
-        btn_m_five.style.color = 'white';
+        btn_m_five.style.backgroundColor = 'yellow';
+        btn_m_five.style.border = '3px dashed red';
+        btn_m_five.style.color = 'black';
         m_five_count = 0;
     }
 });
@@ -240,16 +241,16 @@ var m_ten_count = 0;
 const btn_m_ten = document.getElementById('10m');
 btn_m_ten.addEventListener('click', function onClick() {
     if (m_ten_count == 0){
-        btn_m_ten.style.backgroundColor = 'black';
-        btn_m_ten.style.border = '3px dashed black';
+        btn_m_ten.style.backgroundColor = 'red';
+        btn_m_ten.style.border = '3px dashed yellow';
         btn_m_ten.style.color = 'white';
         m_ten_count = 1;
         //could possibly set the value for db querry here as well 
     }
     else{
-        btn_m_ten.style.backgroundColor = 'black';
-        btn_m_ten.style.border = '3px dashed black';
-        btn_m_ten.style.color = 'white';
+        btn_m_ten.style.backgroundColor = 'yellow';
+        btn_m_ten.style.border = '3px dashed red';
+        btn_m_ten.style.color = 'black';
         m_ten_count = 0;
     }
 });
